e1add0005931fa1109ba3ae4a81cbf4cf3e2e9be
Anders Karlsen <karlsenanders@hotmail.com>
2012-04-19 22:45:24 +0200

    Added correct color

--- src/mastermind/core.clj
+++ src/mastermind/core.clj
@@ -9,3 +9,14 @@
     (+ (if (= (first fact) (first guess)) 1 0) (correct-place (rest fact) (rest guess)))
   )
   )
+
+(defn correct-color [fact guess]
+  (let [map-fact (frequencies fact) 
+        map-guess (frequencies guess)
+        union-keys (fn [master slave] (apply dissoc master (keys (apply dissoc master (keys slave)))))
+        ]
+  (reduce + (vals (merge-with min (union-keys map-fact map-guess) (union-keys map-guess map-fact))))
+  )
+  )
+  
+
--- test/mastermind/test/core.clj
+++ test/mastermind/test/core.clj
@@ -7,3 +7,9 @@
   (is (= 0 (correct-place [:red :red :red :red] [:blue :blue :blue :blue])))
   (is (= 1 (correct-place [:red :red :red :red] [:blue :red :blue :blue])))
   )
+
+(deftest test-correct-color 
+  (is (= 4 (correct-color [:red :red :red :red] [:red :red :red :red])))
+  (is (= 0 (correct-color [:red :red :red :red] [:blue :blue :blue :blue])))
+  (is (= 2 (correct-color [:red :green :blue :blue] [:green :blue :yellow :yellow])))
+  )
